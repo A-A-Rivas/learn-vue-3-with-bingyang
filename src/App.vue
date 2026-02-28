42d75c4e3c3f575d75a3cb13c7505a111375857e
Angel Rivas <angelrivas603@gmail.com>
2026-02-28 12:48:03 -0600

    finished

--- src/App.vue
+++ src/App.vue
@@ -3,36 +3,24 @@
   <img v-bind:src="imageUrl" alt="" />
   <br />
 
-  <!-- shorthand for v-bind -->
   <img :src="imageUrl" alt="" />
-  <br />
 
   <button @click="changeImg">Change image</button>
-
-  <br />
-
   <hr />
-
   <input type="text" :value="defaultInputText" />
 
-  <hr />
-
+  <hr>
   <p :class="className">Harry Potter</p>
 
-  <!-- define a JS object in :class -->
-  <p :class="{ inactive: isInactive, center: isCenter }">
-    <!-- if you think embedding a JS object in HTML is verbose, you can choose to move the object to the script, 
-        give it a name, and only put the JS object name in :class -->
+  <p :class="{inactive: isInactive, center: isCenter}">
     Harry Potter
   </p>
 
-  <!-- define a JS array in :class -->
   <p :class="['active', 'center']">Harry Potter</p>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-
 let message = 'Hello, v-bind!'
 let imageUrl = ref('public/img/banner_1.jpg')
 
@@ -40,10 +28,11 @@ function changeImg() {
   imageUrl.value = 'public/img/banner_2.jpg'
 }
 
-let defaultInputText = 'Write something here...'
+let defaultInputText='Write something here...'
 
-let className = ref('active')
-let isInactive = ref(true)
+let className ='active'
+
+let isInactive = ref(false)
 let isCenter = ref(false)
 </script>
 
