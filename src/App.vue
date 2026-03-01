1ea3f386ca37be43aa44bc8d97e4c477d31bb9c4
Angel Rivas <angelrivas603@gmail.com>
2026-02-28 19:05:39 -0600

    final

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>v-model vs v-bind</h1>
+  <h2>message: {{ message }}</h2>
+  <div>
+    <p>v-model = v-bind + @input</p>
+    <input
+      type="text"
+      v-bind:value="message"
+      @input="message = $event.target.value"
+    />
+  </div>
+  <div>
+    <p>v-model</p>
+    <input type="text" v-model="message" />
+  </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+let message = ref('Hello!')
+</script>
 
 <style scoped></style>
