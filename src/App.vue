efd7a7da6ccb0e1c87f99f507d5c22dc85ce842d
Angel Rivas <angelrivas603@gmail.com>
2026-02-28 19:33:55 -0600

    final commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
+  <h1>{{ message }}</h1>
   <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
+    What has keys but can't open locks, space but no room, and you can enter but
+    can't go outside? What am I?
   </p>
+
+  <button style="width: 200px" @click="showAnswer = !showAnswer">
+    {{ !showAnswer ? 'Show me the answer!' : 'Hide the answer!' }}
+  </button>
+
+  <div v-if="showAnswer">A keyboard.</div>
+
+  <div v-show="showAnswer">A keyboard.</div>
+
+ <div v-if="Math.random() > 0.5">Now you see me</div>
+  <div v-else>Now you don't</div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+let message = ref('Hello, v-if!')
+
+let showAnswer = ref(false)
+</script>
 
 <style scoped></style>
