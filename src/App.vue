be96d60fc497713d5da86a9fea6918db9d09189e
Angel Rivas <angelrivas603@gmail.com>
2026-03-10 11:14:46 -0500

    finished

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <AlertBox>Invalid username or password. Please try again</AlertBox>
+  <AlertBox></AlertBox>
+  <AlertBox>
+    <h1>The request timed out. Please refresh the page and try again</h1>
+  </AlertBox>
+</template>
+
+<script setup>
+import AlertBox from './AlertBox.vue'
+
+</script>
+
+<style scoped>
+
+</style>
